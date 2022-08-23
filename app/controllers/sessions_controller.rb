780b01eb434bc68099d5ace5cd02d1d0680c29cb
Victoria LeBel <vickileb@hotmail.com>
2022-08-23 10:09:24 -0700

    fixed syntax error in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
       session[:user_id]= user.id
       render json: user, status: :ok
     else
-      render json: "Invalid Username or Password", status: :unauthorized
+      render json: {errors: ["Invalid Username or Password"]}, status: :unauthorized
     end
   end
 
--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "./views/Home";
 import Navigation from "./components/Navigation";

c09bd2aab40f2eed1a24f9c8791824dbf9862e87
Victoria LeBel <vickileb@hotmail.com>
2022-08-23 18:47:58 -0700

    individual restaurant pages render successfully

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -7,7 +7,7 @@ class RestaurantsController < ApplicationController
     end
 
     def show
-        restaurant= Restaurant.find_by(params[:id])
+        restaurant= Restaurant.find_by(id: params[:id])
         render json: restaurant
     end
 end
--- client/src/views/RestaurantDetails.js
+++ client/src/views/RestaurantDetails.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { useParams } from 'react-router-dom'
 
 function RestaurantDetails() {
+    const [restaurant, setRestaurant] = useState({})
+    const [errors, setErrors] = useState([])
+
+    const params = useParams()
+
+    useEffect(()=>{
+        fetch(`/restaurants/${params.id}`)
+        .then((resp)=>{
+            if(resp.ok){
+                resp.json().then(data => setRestaurant(data))
+            }else{
+                resp.json().then((data)=> setErrors(data.error))
+            }
+        })
+    },[])
+
+
   return (
     <div>
-        <h1>Restaurant Details here</h1>
+        <h1>Welcome to {restaurant.name}!</h1>
+        <img alt="food image" src={restaurant.image_url}/>
     </div>
   )
 }

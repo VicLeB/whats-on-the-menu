adf73eb8817dfd12d553502b7a8cd9df829290ec
Victoria LeBel <vickileb@hotmail.com>
2022-08-26 00:26:45 -0700

    can add and delete a menu

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -10,4 +10,21 @@ class MenusController < ApplicationController
         menu= Menu.find_by(id: params[:id])
         render json: menu, include: ['courses', 'courses.menu_items']
     end
+
+    def create
+        menu =Menu.create!(menu_params)
+        render json: menu, status: :created
+    end
+
+    def destroy
+        menu= Menu.find_by(id: params[:id])
+        menu.destroy
+        head :no_content
+    end
+
+    private
+
+    def menu_params
+        params.permit(:name, :theme, :restaurant_id)
+    end
 end

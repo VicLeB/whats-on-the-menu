7080ebfd9289f7fdb43d51084d8f4ac30b18f779
Victoria LeBel <vickileb@hotmail.com>
2022-09-29 18:05:52 -0700

    introduced styling to login screen

--- client/src/views/Login.js
+++ client/src/views/Login.js
@@ -1,32 +1,59 @@
 import React from 'react'
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import styled from "styled-components";
 import LoginForm from '../components/LoginForm';
 import SignUpForm from '../components/SignUpForm';
+import Button from 'react-bootstrap/Button';
 
 function Login({onLogin}) {
     const [showLogin, setShowLogin] = useState(true)
 
   return (
-    <Wrapper>
+    <LoginDiv>
         {showLogin? (
-            <>
+          <Wrapper>
+              <WelcomeTitle>Welcome Back!</WelcomeTitle>
         <LoginForm onLogin= {onLogin}/>
-        <p>Create an account <button onClick={()=> setShowLogin(false)}>Sign up</button>
-        </p>
-        </>
+        <AccountStatusCaption>Don't have an account? Create one here <Button variant='primary' onClick={()=> setShowLogin(false)}>Sign up</Button>
+        </AccountStatusCaption>
+        </Wrapper>
         ) : (
-            <>
+            <Wrapper>
         <SignUpForm onLogin={onLogin}/>
-        <p>Already have an account? <button onClick={()=> setShowLogin(true)}>Sign in</button></p>
-        </>
+        <p>Already have an account? <Button variant='primary' onClick={()=> setShowLogin(true)}>Sign in</Button></p>
+        </Wrapper>
         )}
-    </Wrapper>
+    </LoginDiv>
   )
 }
 
 export default Login
 
+const AccountStatusCaption = styled.h6`
+font-weight: normal;
+padding-bottom: 5vh;
+`
+
+const WelcomeTitle = styled.h1`
+  margin:0 ;
+  padding-top: 5vh;
+`
+
+const LoginDiv = styled.div`
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+`
+
 const Wrapper = styled.div`
-font-family: 'Courier New', Courier, monospace;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  border: 2px solid lightgrey;
+  border-radius: 5px;
+  height: 50vh;
+  width: 50vw;
+  box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.2);
+
 `
